a4510f543166e53f551cd79d6d8dd3fc970f51d7
Aryan <126262477+builtbyaryan@users.noreply.github.com>
2025-08-30 09:42:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'weather-app'  // 👈 very important
+  base: '/weather-app/'   // 👈 must have leading + trailing slash
 })
